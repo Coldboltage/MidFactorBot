569f63b6eefc6e19b7be0ba21c59850b19e7995a
Coldboltage <alanreid@hotmail.co.uk>
2022-04-16 18:13:35 +0100

    first commit

---
+++ puppeteer/individual/getJsonData.puppeteer.js
@@ -0,0 +1,46 @@
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+const axios = require("axios");
+const rp = require("request-promise");
+
+
+const getJsonData = async (page, name, url, httpRequest) => {
+  // Where the emitter results go into
+  const result = []
+  page.on("request", async (request) => {
+    const test = request.url();
+    // Use Axios
+    if (
+      test.includes(name) &&
+      !test.includes("static") &&
+      httpRequest === "axios"
+    ) {
+      console.log(test);
+      const response = await axios(test);
+      const { matches } = response.data.pageProps.data;
+      result.push(matches);
+    }
+    // Use Request
+    else if (
+      test.includes(name) &&
+      !test.includes("static") &&
+      httpRequest === "request"
+    ) {
+      console.log(test);
+      try {
+        const response = await rp.get(test);
+        const data = JSON.parse(response);
+        // console.log(data.data)
+        result.push(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
+  await page.goto(url);
+  // Destructure result array as it's own array so no array inception
+  const [extract] = result
+  return extract
+};
+
+module.exports = getJsonData
