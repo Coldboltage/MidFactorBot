fc5efad96d6b46d018735c1be7f27cbaca2ba286
Coldboltage <alanreid@hotmail.co.uk>
2022-04-17 13:36:43 +0100

    added more structure
    I expect I'll have frontend and backend functionality so I'm setting up the server

---
+++ server/src/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const checkMidniteFactorData = require("./puppeteer/bundle/checkMidniteFactorData.puppeteer")
+
+const server = http.createServer();
+
+const startServer = async () => {
+  checkMidniteFactorData()
+}
+
+startServer()
