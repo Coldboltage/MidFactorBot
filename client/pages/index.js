7a23870a7e0d500c05ecb069379cfea0db3d0ffa
unknown <alandreid@hotmail.co.uk>
2022-04-26 00:10:31 +0100

    removed semantic will figure something out

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,40 +6,7 @@ const index = ({data}) => {
   console.log(data)
   return (
     <div>
-      <h1>{`I don't like this`}</h1>
-      <Button>Click Here</Button>
-      <Table celled>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell className={styles.headerCell}>Home Team Name</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Home Team Prediction</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Home Team Odds</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Away Team Name</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Away Team Prediction</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Away Team Odds</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Match Start</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Factor ID</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>MidniteMatch ID</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Upcoming</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Bet Setup</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Bet Placed</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Team To Win</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Bankroll</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Odds</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Prediction</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Time To Bet</Table.HeaderCell>
-            <Table.HeaderCell className={styles.headerCell}>Won</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
+      <h1>This be some work here</h1>
     </div>
   );
 };
