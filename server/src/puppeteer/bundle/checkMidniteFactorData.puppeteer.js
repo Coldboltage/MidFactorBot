46f1bd8f53caceb98a60653a18626e2a8529ca94
Coldboltage <alanreid@hotmail.co.uk>
2022-04-30 11:51:56 +0100

    false head

--- server/src/puppeteer/bundle/checkMidniteFactorData.puppeteer.js
+++ server/src/puppeteer/bundle/checkMidniteFactorData.puppeteer.js
@@ -16,7 +16,7 @@ const { matchFactorToMidniteGames } = require("../../models/matches.model");
 const main = async () => {
   // const browser = await puppeteer.launch({headless:false})
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
